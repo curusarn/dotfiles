335b79f9f12070c5d307ad9280ca757a635a61de
Simon Let <simon.a.let@gmail.com>
2017-09-08 18:57:21 +0200

    Change setup script

--- .setup/antergos.sh
+++ .setup/antergos.sh
@@ -6,16 +6,17 @@ echo "Updating system"
 sudo pacman -Syu
 
 echo "Install packages"
-bin/pacsave load core pcmanfm spotify utils video
+~/dotfiles-tmp/bin/pacsync install
 
 echo "Copying files"
 cp -vr $base_path/files/.??* ~/
-
-echo "Bg image"
-~/.setup/bg_image.sh
 
 echo "Enable NetworkManager.service"
 sudo systemctl enable NetworkManager.service
 
 echo "Initialize dotfiles"
 .dotfiles/init.sh
+
+echo "Bg image"
+~/.setup/bg_image.sh
+
