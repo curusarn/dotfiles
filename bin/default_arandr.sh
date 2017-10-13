1e1e6fd6e4fb78b37826a2bf2dfd94afc11b1abb
Šimon Let <simon.a.let@gmail.com>
2017-10-13 12:04:04 +0200

    Add comments

--- bin/default_arandr.sh
+++ bin/default_arandr.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# load default screenlayout if set
+# aka run `~/.screenlayout/default.sh` if (is executable and) exists
+
 [[ -x ~/.screenlayout/default.sh ]] && ~/.screenlayout/default.sh
 
