788dee30f22169725493d9b3d4c7073cb8329358
Simon Let <simon.a.let@gmail.com>
2019-08-26 09:15:11 +0200

    detect distro - manjaro

--- bin/detect-distro.sh
+++ bin/detect-distro.sh
@@ -4,7 +4,7 @@ distId=`lsb_release -a 2>&1 |grep "Distributor ID" | \
         cut -d':' -f2 |tr -d ' \t'|tr '[:upper:]' '[:lower:]'`
 
 case $distId in
-    arch|antergos)
+    arch|antergos|manjarolinux)
         echo arch
         exit 0
     ;;
